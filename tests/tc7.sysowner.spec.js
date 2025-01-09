5f525083e477ebea786ae1aa12be983c5eb7422b
Vishal Paliwal <vishal.paliwal@gmail.com>
2025-01-09 18:20:31 +0800

    updated tc7

--- tests/tc7.sysowner.spec.js
+++ tests/tc7.sysowner.spec.js
@@ -1,8 +1,13 @@
 import { test, expect } from "@playwright/test";
-import { baseURL } from "../utils/testData";
+import { baseURL, createValidVoucherPayload } from "../utils/testData";
 
 test.describe('User Story 6: Voucher category and number API Tests', () => {
+    const payLoad = createValidVoucherPayload()
+
     test('should return number of vouchers each customer has for each voucher category', async ({ request }) => {
+        await request.post(`${baseURL}`+'/api/v1/hero/vouchers', {
+            data: payLoad
+        });
         const response = await request.get(`${baseURL}`+'/api/v1/voucher/by-person-and-type');
 
         const responseBody = await response.json();
